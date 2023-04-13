5aa90938aa09ec55ae93d453bf005e89ae94eae4
Jairo Sánchez <jairosanchezb5@gmail.com>
2023-04-13 19:19:57 +0200

    subida de proyecto dos prueba ia

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { sendMessageToOpenAI } from "./axios";
+import "./App.css";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const handleMessageSubmit = async () => {
+    const response = await sendMessageToOpenAI(input);
+    setMessages([
+      ...messages,
+      { text: input, isUser: true },
+      { text: response, isUser: false },
+    ]);
+    setInput("");
+  };
+
+  return (
+    <div className="App">
+      <div className="chat">
+        {messages.map((message, index) => (
+          <div
+            key={index}
+            className={message.isUser ? "user-message" : "bot-message"}
+          >
+            {message.text}
+          </div>
+        ))}
+      </div>
+      <div className="input-container">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={handleMessageSubmit}>Enviar</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
